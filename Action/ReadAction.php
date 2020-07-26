65e480f3798ef9dfa804759d2535491385ba0fba
Philip Washington Sorst <philip@sorst.net>
2020-07-26 14:00:03 +0200

    Making ReadAction usable

--- Action/ReadAction.php
+++ Action/ReadAction.php
@@ -10,26 +10,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ReadAction
-{private CrudAdminService $crudAdminService;
+{
+    private CrudAdminService $crudAdminService;
 
     public function __construct(CrudAdminService $crudAdminService)
     {
         $this->crudAdminService = $crudAdminService;
     }
+
     public function __invoke(Request $request): Response
     {
-        $crudAdminRequest = new CrudAdminRequest(CrudOperation::LIST, $request);
+        $crudAdminRequest = new CrudAdminRequest(CrudOperation::READ, $request);
         $entity = $this->crudAdminService->getEntity($crudAdminRequest);
         if (null === $entity) {
             throw new NotFoundHttpException();
         }
         $this->crudAdminService->checkAuthorization($crudAdminRequest);
         $template = $this->crudAdminService->getTemplate($crudAdminRequest);
+        $title = $this->crudAdminService->getTitle($crudAdminRequest);
+        $routes = $this->crudAdminService->getRoutes($crudAdminRequest);
+        $fieldDefinitions = $this->crudAdminService->getFieldDefinitions($crudAdminRequest);
 
         return $this->crudAdminService->render(
             $template,
             [
-                'entity' => $entity
+                'title'            => $title,
+                'entity'           => $entity,
+                'routes'           => $routes,
+                'fieldDefinitions' => $fieldDefinitions
             ]
         );
     }
