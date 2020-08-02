2dc88cdebce687375310028cc1df207c2b0f493a
Philip Washington Sorst <philip@sorst.net>
2020-08-02 17:52:26 +0200

    Adding more field renderers

--- Twig/CrudAdminExtension.php
+++ Twig/CrudAdminExtension.php
@@ -2,10 +2,12 @@
 
 namespace Dontdrinkandroot\CrudAdminBundle\Twig;
 
+use DateTimeInterface;
 use Dontdrinkandroot\CrudAdminBundle\Model\FieldDefinition;
 use Dontdrinkandroot\CrudAdminBundle\Service\Id\IdResolver;
 use Dontdrinkandroot\CrudAdminBundle\Service\Url\UrlResolver;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Intl\DateFormatter\IntlDateFormatter;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
@@ -23,8 +25,12 @@ class CrudAdminExtension extends AbstractExtension
 
     private RequestStack $requestStack;
 
-    public function __construct(PropertyAccessor $propertyAccessor, IdResolver $idResolver, UrlResolver $urlResolver, RequestStack  $requestStack)
-    {
+    public function __construct(
+        PropertyAccessor $propertyAccessor,
+        IdResolver $idResolver,
+        UrlResolver $urlResolver,
+        RequestStack $requestStack
+    ) {
         $this->propertyAccessor = $propertyAccessor;
         $this->idResolver = $idResolver;
         $this->urlResolver = $urlResolver;
@@ -52,6 +58,27 @@ class CrudAdminExtension extends AbstractExtension
     public function renderFieldDefinitionValue(object $entity, FieldDefinition $fieldDefinition)
     {
         $value = $this->propertyAccessor->getValue($entity, $fieldDefinition->getPropertyPath());
+        if (null === $value) {
+            return '<i>null</i>';
+        }
+
+        switch ($fieldDefinition->getType()) {
+            case 'datetime':
+                assert(
+                    $value instanceof DateTimeInterface,
+                    $fieldDefinition->getPropertyPath() . ' was not a DateTimeInterface'
+                );
+                return $value->format('Y-m-d H:i:s');
+            case 'date':
+                assert(
+                    $value instanceof DateTimeInterface,
+                    $fieldDefinition->getPropertyPath() . ' was not a DateTimeInterface'
+                );
+                return $value->format('Y-m-d');
+            case 'boolean';
+                return $value ? '<span class="fas fa-check"></span>' : '<span class="fas fa-times"></span>';
+        }
+
         $value = htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
 
         return $value;
