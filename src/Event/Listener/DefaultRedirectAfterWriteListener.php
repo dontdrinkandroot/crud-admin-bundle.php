569678cd4b872597510088fcaa7662187f44742c
Philip Washington Sorst <philip@sorst.net>
2024-02-14 23:53:17 +0100

    fix: Using Translatable message in FlashBag

--- src/Event/Listener/DefaultRedirectAfterWriteListener.php
+++ src/Event/Listener/DefaultRedirectAfterWriteListener.php
@@ -9,6 +9,7 @@ use Dontdrinkandroot\CrudAdminBundle\Service\Url\UrlResolver;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DefaultRedirectAfterWriteListener
@@ -16,15 +17,17 @@ class DefaultRedirectAfterWriteListener
     public function __construct(
         private readonly UrlResolver $urlResolver,
         private readonly AuthorizationCheckerInterface $authorizationChecker,
-        private readonly TranslatorInterface $translator
     ) {
     }
 
     public function onRedirectAfterWrite(RedirectAfterWriteEvent $event): void
     {
-        Asserted::instanceOf($event->request->getSession(), Session::class)->getFlashBag()->set(
+        Asserted::instanceOf($event->request->getSession(), Session::class)->getFlashBag()->add(
             'success',
-            $this->translator->trans(id: 'success.' . strtolower($event->crudOperation->value), domain: 'DdrCrudAdmin')
+            new TranslatableMessage(
+                message: 'success.' . strtolower($event->crudOperation->value),
+                domain: 'DdrCrudAdmin'
+            )
         );
 
         $redirectUrl = $this->urlResolver->resolveUrl($event->entityClass, CrudOperation::LIST, $event->entity);
