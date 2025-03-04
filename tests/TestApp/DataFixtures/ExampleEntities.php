78b9d4fe7a9a9bb37a3b0912fbd12583ce75a181
Philip Washington Sorst <philip@sorst.net>
2025-03-04 23:30:10 +0100

    tests: Fixing type

--- tests/TestApp/DataFixtures/ExampleEntities.php
+++ tests/TestApp/DataFixtures/ExampleEntities.php
@@ -13,7 +13,7 @@ class ExampleEntities extends Fixture
     public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 20; $i++) {
-            $suffix = str_pad((string)$i, 5, 0, STR_PAD_LEFT);
+            $suffix = str_pad((string)$i, 5, '0', STR_PAD_LEFT);
             $exampleEntity = new ExampleEntity(
                 requiredReadonly: 'requiredReadonly' . $suffix,
                 required: 'required' . $suffix,
